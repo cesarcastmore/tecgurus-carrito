a6e7da7c7e1f989fe1fe4c966070642067c58b13
Cesar Castillo <ccastillo@datafierce.com>
2022-09-30 18:45:14 -0500

    Crear un componente menú con dos inputs: title y path y tomar el nodo “a” del component MenuListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuListComponent } from './components/menu-list/menu-list.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './pages/login/login.component';
     MenuListComponent,
     ProfileComponent,
     ProductsComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
