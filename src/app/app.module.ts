317e1d199eea692c29253d3194b6430cdc974404
Cesar Castillo <ccastillo@datafierce.com>
2022-09-30 18:16:19 -0500

    Crear un componente de perfil de ruta llamado profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuListComponent } from './components/menu-list/menu-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuListComponent
+    MenuListComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
