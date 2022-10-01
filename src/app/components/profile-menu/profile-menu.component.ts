7b602efec829183f0d6e6c1869b26363fea97774
Cesar Castillo <ccastillo@datafierce.com>
2022-09-30 19:27:20 -0500

    Crear un output en profile-menu que regrese la etiqueta donde se le hizo clic en el botón.

--- src/app/components/profile-menu/profile-menu.component.ts
+++ src/app/components/profile-menu/profile-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-profile-menu',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileMenuComponent implements OnInit {
 
+  onTab: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  menu(identifier: string){
+
+    this.onTab.emit(identifier);
+
   }
 
 }
