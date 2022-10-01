7e4ef98300386a1d49fc1f342067303a59196e2c
Cesar Castillo <ccastillo@datafierce.com>
2022-09-30 19:18:56 -0500

    Crear un output en profile-tabs donde regrese una etiqueta de la pestaña donde se le hizo clic

--- src/app/components/profile-tabs/profile-tabs.component.ts
+++ src/app/components/profile-tabs/profile-tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-profile-tabs',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileTabsComponent implements OnInit {
 
+  @Output() onTab:EventEmitter<string> = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  tab(identifier: string){
+    this.onTab.emit(identifier);
   }
 
 }
